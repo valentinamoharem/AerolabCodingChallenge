1806ad7e9b7a00e2aa9b4446d411420415fcba63
valentinamoharem <valentina.moharem@gmail.com>
2019-09-03 14:27:30 -0300

    second commit

--- src/Components/PointShop.jsx
+++ src/Components/PointShop.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import '../Styles/PointShop.css'
 
+import coin from '../Static/icons/coin.svg'
+
 const PointShop = props => {
     
     const [selected, setSelected] = useState(0)
@@ -10,25 +12,28 @@ const PointShop = props => {
     return (
         <div className="black-space" >
             <div className="shop-modal">
-                <div className="shop-modal-selector">
-                    <p>Select the amount of points:</p>
-                    <div className="point-selector">
-                        <p className={`shop-option ${selected === 0 && "selected"}`} onClick={() => setSelected(0)}>1000</p>
-                        <p className={`shop-option ${selected === 1 && "selected"}`} onClick={() => setSelected(1)}>5000</p>
-                        <p className={`shop-option ${selected === 2 && "selected"}`} onClick={() => setSelected(2)}>7500</p>
+                <div className="row-1">
+                    <div className="shop-modal-selector">
+                        <p>Select amount of points:</p>
+                        <div className="point-selector">
+                            <div className="frst-row">
+                                <p className={`shop-option ${selected === 0 && "selected"}`} onClick={() => setSelected(0)}>1000</p>
+                                <p className={`shop-option ${selected === 1 && "selected"}`} onClick={() => setSelected(1)}>5000</p>
+                            </div>
+                            <p id="lst-row" className={`shop-option ${selected === 2 && "selected"}`} onClick={() => setSelected(2)}>7500</p>
+                        </div>
+                    </div>
+                    <div className="shop-modal-balance">
+                        <p>Total points:</p>
+                        <div className="balance">
+                            <p>{`${props.points + optionArr[selected]}`}</p>
+                            <img className='coin' src={coin} alt="coin"/>
+                        </div>
                     </div>
                 </div>
-                <div className="shop-modal-balance">
-                    <div className="balance">
-                        <h5>Your balance:</h5>
-                        <p>{`Your points.... ${props.points}`}</p>
-                        <p>{`Your order..... ${optionArr[selected]}`}</p>
-                        <p>{`Total points... ${props.points + optionArr[selected]}`}</p>
-                    </div>
-                    <div className="buttons">
+                <div className="row-2">
                         <button className="cancel" onClick={() => props.togglePointShop()}>Cancel</button>
                         <button className="confirm" onClick={() => props.addPoints(optionArr[selected])}>Confirm</button>
-                    </div>
                 </div>
             </div>
         </div>
